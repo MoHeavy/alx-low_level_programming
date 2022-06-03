2f2e72dd03f5c619106627f1c3950d538edd7c03
MoHeavy <konemoheavy@gmail.com>
2022-06-03 00:05:07 -0700

    Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
+#include<stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - the entry point
+ * Description: 'and tat piece of art is useful" - Dora korpar, 2015-10-19'
+ * Return: 1
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+ 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",);
 
-	write(2, str1, 59);
 	return (1);
+}
