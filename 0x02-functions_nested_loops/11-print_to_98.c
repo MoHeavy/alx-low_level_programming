5d67e1285f9e04a4cf5a4c3c86d44455ab784a6f
MoHeavy <konemoheavy@gmail.com>
2022-06-08 04:58:18 -0700

    Noma

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98.
+ * @n: number to start counting from
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%i, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%i, ", n);
+		n--;
+	}
+	printf("98");
+	putchar('\n');
+}
