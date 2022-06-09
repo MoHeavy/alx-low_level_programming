1747e7bbbe39a41b613c65192f753d83c98071aa
MoHeavy <konemoheavy@gmail.com>
2022-06-09 05:51:24 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers btween 0-9
+ */
+void print_numbers(void)
+{
+	int k;
+
+	for (k = 48; k < 58; k++)
+
+	{
+		_putchar(k);
+	}
+	_putchar('n\');
+}
+
