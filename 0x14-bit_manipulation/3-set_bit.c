16f0029f61fa75e5cff3d484f2ecf98f9dc12335
MoHeavy <konemoheavy@gmail.com>
2022-07-22 00:28:59 -0700

    Sets the valur
    {
    }{
    {}
    }{}{]q[sq]s
    git commit -m Sets the value of bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - sets value of bit to 1
+ * @*n: pointer of the int
+ * @index: index of bit
+ * Return: 1 if it worked, -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int h;
+
+	if (index > 63)
+		return (-1);
+
+	h = 1 << index;
+	*n = (*n | h);
+
+	return (1);
+}
